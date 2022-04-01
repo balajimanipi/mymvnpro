e24ccad939f0b1a59c3937eb4bd42bde6dd93da5
balajimanipi <mbalajimanapi@gmail.com>
2022-04-01 09:22:26 +0530

    inital code

---
+++ trishanawebapp/src/main/java/com/trishana/App.java
@@ -0,0 +1,13 @@
+package com.trishana;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
